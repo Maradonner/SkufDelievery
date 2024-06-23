541cb0b809cb4e2d56e13a1102a5b9449c1d9df9
DaniilReller <141248625+DaniilReller@users.noreply.github.com>
2024-06-24 00:24:40 +0500

    Add cart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {ShopList} from "./pages/shop/ShopList.tsx";
 import React from "react";
+import {Cart} from "./pages/cart/Cart.tsx";
 
 
 function App() {
     return (
         <>
+            <Cart></Cart>
             <ShopList></ShopList>
         </>
     )
